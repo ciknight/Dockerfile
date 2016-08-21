3f14ba0d33bdf562f1b86287708e88b245b53640
hainingw <hainingw@yishow.me>
2016-08-21 14:15:20 +0000

    Update ciproxy

--- ciproxy/ciproxy.sh
+++ ciproxy/ciproxy.sh
@@ -1,4 +1,4 @@
-ssserver -c /etc/shadowsocks.json -d start
+ssserver -c /etc/shadowsocks.json -d start --log-file /var/log/ssserver.log --pid-file /var/run/ssserver.pid
 sslocal -d start -s 127.0.0.1 -p 11200 -b 127.0.0.1 -l 9001 -k password -m aes-128-cfb --pid-file /var/run/shadowsocks_1.pid
 sslocal -d start -s 127.0.0.1 -p 11200 -b 127.0.0.1 -l 9002 -k password -m aes-128-cfb --pid-file /var/run/shadowsocks_2.pid
 haproxy -D -f /etc/haproxy/haproxy.cfg > /var/log/haproxy.log
