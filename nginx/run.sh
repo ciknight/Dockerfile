233bb3fccf1cc57a8521ea7dcaf62dc0cc2772bc
hainingw <hainingw@yishow.me>
2016-08-28 04:43:32 +0000

    fix docker error

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,8 +1,10 @@
 # Start Nginx 
+PWD=`pwd`
 
 docker stop nginx && docker rm nginx
 docker run -it -d --name nginx \
     -p 80:80 \
     -p 443:443 \
-    -v /etc/nginx/conf.d:conf.d \
+    -v $PWD/conf.d:/etc/nginx/conf.d \
+    -v $PWD/pages:/usr/share/nginx/blog.ibeats.top \
     nginx:1.9.15
