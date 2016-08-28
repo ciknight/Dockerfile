056896d6954450c03e3729c8ac288077694e1255
hainingw <hainingw@yishow.me>
2016-08-28 09:05:08 +0000

    optimized

--- nginx/run.sh
+++ nginx/run.sh
@@ -6,5 +6,6 @@ docker run -it -d --name nginx \
     -p 80:80 \
     -p 443:443 \
     -v $PWD/conf.d:/etc/nginx/conf.d \
+    -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
     -v $PWD/pages:/usr/share/nginx/blog.ibeats.top \
     nginx:1.9.15
