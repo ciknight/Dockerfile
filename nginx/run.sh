9403a5d8e04d3548ad8f40ca34a823b5a7c1e83b
hainingw <hainingw@yishow.me>
2016-08-28 02:30:40 +0000

    fix eror

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,5 +4,5 @@ docker stop nginx && docker rm nginx
 docker run -it -d --name nginx \
     -p 80:80 \
     -p 443:443 \
-    -v /etc/nginx/conf.d:/conf.d \
+    -v /etc/nginx/conf.d:conf.d \
     nginx:1.9.15
