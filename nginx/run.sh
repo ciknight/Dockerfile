c3d55df26b4397a56ce9886a92b20c205ef24e78
hainingw <hainingw@yishow.me>
2016-09-20 03:06:59 +0000

    fix nginx default page

--- nginx/run.sh
+++ nginx/run.sh
@@ -5,7 +5,7 @@ docker stop nginx && docker rm nginx
 docker run -it -d --name nginx \
     -p 80:80 \
     -p 443:443 \
-    -v $PWD/conf.d:/etc/nginx/conf.d \
+    -v $PWD/sites-enabled:/etc/nginx/sites-enabled\
     -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
     -v $PWD/pages:/usr/share/nginx/blog.ibeats.top \
     nginx:1.9.15
