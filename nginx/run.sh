cf638e916db1ab6983c0ffdb68c8fbf1a06e915c
hainingw <hainingw@yishow.me>
2016-10-23 09:29:23 +0000

    update conf

--- nginx/run.sh
+++ nginx/run.sh
@@ -6,6 +6,8 @@ docker run -it -d --name nginx \
     -p 80:80 \
     -p 443:443 \
     -v $PWD/sites-enabled:/etc/nginx/sites-enabled\
+    -v $PWD/cert:/etc/nginx/cert\
     -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
     -v $PWD/pages:/usr/share/nginx/blog.ibeats.top \
+    -v /tmp:/tmp \
     nginx:1.9.15
