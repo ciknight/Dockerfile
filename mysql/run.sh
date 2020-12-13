7d460ac977a23008335933aeaa87313031533e98
Andy Wang <ci_knight@msn.cn>
2020-12-13 09:53:35 +0800

    feat: update mysql dockerfile

---
+++ mysql/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+docker stop mysql && docker rm mysql
+docker run -itd --name mysql \
+    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+    -e MYSQL_ROOT_PASSWORD=root \
+    -e MYSQL_DATABASE=mysql \
+    -v /Users/andy/opt/mysql/datadir:/var/lib/mysql \
+    -p 3306:3306 \
+    mysql:8.0.21 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
